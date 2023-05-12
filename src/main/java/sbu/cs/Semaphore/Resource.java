87bd33ab1b3a5b965588a435c63674fe36b81787
Shaghayegh-Dianat <shaghayeghflower79@gmail.com>
2023-05-12 20:38:31 +0330

    add a semaphore from Semaphore class to Controller class and give it to all operators

--- src/main/java/sbu/cs/Semaphore/Resource.java
+++ src/main/java/sbu/cs/Semaphore/Resource.java
@@ -1,8 +1,13 @@
 package sbu.cs.Semaphore;
+import java.text.SimpleDateFormat;
 
 public class Resource {
+    SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
+
+
 
     public static void accessResource() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
