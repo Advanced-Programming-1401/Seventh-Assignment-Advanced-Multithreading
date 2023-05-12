e0857dc5f5418b7ed0c146444396d8142c7a3c69
Shaghayegh-Dianat <shaghayeghflower79@gmail.com>
2023-05-12 21:53:32 +0330

    handle race condition by semaphore

--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -13,7 +13,14 @@ public class Operator extends Thread {
     public void run() {
         for (int i = 0; i < 10; i++)
         {
-            Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
+            try {
+                semaphore.acquire();
+                Resource.accessResource();// critical section - a Maximum of 2 operators can access the resource concurrently
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
             try {
                 sleep(500);
             } catch (InterruptedException e) {
