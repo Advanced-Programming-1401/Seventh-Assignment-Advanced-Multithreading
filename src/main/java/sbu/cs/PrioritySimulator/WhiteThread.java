5b56699dd7d5a35f149745cbeb74b100bb89fc47
Shaghayegh-Dianat <shaghayeghflower79@gmail.com>
2023-05-12 18:27:29 +0330

    complete run method of WhiteThread class

--- src/main/java/sbu/cs/PrioritySimulator/WhiteThread.java
+++ src/main/java/sbu/cs/PrioritySimulator/WhiteThread.java
@@ -20,6 +20,7 @@ public class WhiteThread extends ColorThread {
 
     @Override
     public void run() {
-        // TODO call printMessage
+        printMessage();
+        CDL.countDown();
     }
 }
