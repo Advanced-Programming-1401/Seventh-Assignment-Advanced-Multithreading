43ce9cea3bf026702fdeb8bd880e6857a808dc41
Shaghayegh-Dianat <shaghayeghflower79@gmail.com>
2023-05-12 21:43:47 +0330

    define a simple format and change sleep time to 1000 to understand the result better

--- src/main/java/sbu/cs/Semaphore/Resource.java
+++ src/main/java/sbu/cs/Semaphore/Resource.java
@@ -2,14 +2,15 @@ package sbu.cs.Semaphore;
 import java.text.SimpleDateFormat;
 
 public class Resource {
-    SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
+    SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");//use this form to convert the time format from millisecond
 
 
 
     public static void accessResource() {
         SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
         try {
-            Thread.sleep(100);
+            System.out.println(Thread.currentThread().getName() + " accessed to the resource at " + sdf.format(System.currentTimeMillis()));
+            Thread.sleep(1000);//change it to 1000 understand that every second just two threads can access the resource
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
