602f0629f301ef6bf8cdfca2e5b7741cd1e233a8
Shaghayegh-Dianat <shaghayeghflower79@gmail.com>
2023-05-13 02:48:59 +0330

    complete PICalculator class

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,5 +1,12 @@
 package sbu.cs.CalculatePi;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class PiCalculator {
 
     /**
@@ -15,10 +22,63 @@ public class PiCalculator {
      * @return pi in string format (the string representation of the BigDecimal object)
      */
 
+    public static BigDecimal pi = new BigDecimal(0);
+
+    public static class TermCalculator implements Runnable {
+        MathContext mc = new MathContext(1000);
+        BigDecimal numerator = new BigDecimal(4);
+        int floatingPoint;
+        int n; // number of the term
+
+        public TermCalculator (int n, int floatingPoint) {
+            this.n = n;
+            this.floatingPoint = floatingPoint;
+        }
+
+        public void run(){
+            BigDecimal sign = new BigDecimal(1);
+            if (n%2 == 1) {
+                sign = new BigDecimal(-1);
+            }
+
+            BigDecimal term = new BigDecimal(1);
+            term = term.multiply(sign, mc);
+            term = term.multiply(numerator, mc);
+            term = term.divide(denominator(n), mc);
+            addTerm(term, mc);
+        }
+
+        private BigDecimal denominator (int n){
+            BigDecimal den = new BigDecimal(Integer.toString((2*n + 2)*(2*n+3)*(2*n+4)));
+            return den;
+        }
+    }
+
+    public static synchronized void addTerm(BigDecimal term, MathContext mc){
+        pi = pi.add(term, mc);
+    }
+
     public String calculate(int floatingPoint)
     {
-        // TODO
-        return null;
+        pi = new BigDecimal(3);
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
+        final int iterations = 10000;
+        for (int i = 0; i <= iterations; i++){
+            TermCalculator tc = new TermCalculator(i, floatingPoint);
+            threadPool.execute(tc);
+        }
+
+        threadPool.shutdown();
+
+        try {
+            threadPool.awaitTermination(5, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pi = pi.setScale(floatingPoint, RoundingMode.HALF_UP);
+        return pi.toPlainString();
     }
 
     public static void main(String[] args) {
